5965c1411d5ad69c7536729f559f695f7f0bbb27
Lucas Bonomi <lucas.bonomi@gmail.com>
2014-08-28 15:48:56 -0700

    Update jqdebug.js

--- jqdebug.js
+++ jqdebug.js
@@ -6,19 +6,19 @@
 
 function debug($rule){
   var debug = $('[data-debug]');
-  
+
   debug.each(function(){
-		var $this = $(this);
-		var debugColor = $this.attr('data-debug');
+    var $this = $(this);
+    var debugColor = $this.attr('data-debug');
 
     if($rule == 'box-shadow'){
-       $this.css($rule,'0 0 1em ' + debugColor);
+      $this.css($rule,'0 0 1em ' + debugColor);
     }
     else if($rule == 'border'){
-       $this.css($rule,'1px solid ' + debugColor);
+      $this.css($rule,'1px solid ' + debugColor);
     }
     else{
-       $this.css($rule, debugColor);
+      $this.css($rule, debugColor);
     }
   });
 } 
