0e0e6b78b4cbb9094c017b83502d23998e98f7d1
Lucas Bonomi <lucas.bonomi@gmail.com>
2014-08-28 13:25:28 -0700

    Create jqdebug.js

---
+++ jqdebug.js
@@ -0,0 +1,28 @@
+/* 
+  jqDebug.js | A tiny jquery debug function.
+  Author : @LukyVj
+  Date : Aug 2014
+*/
+
+$(function(){
+  
+  function debug($rule){
+    var debug = $('[data-debug]');
+    var debugColor = debug.attr('data-debug');
+    
+    debug.each(function(){
+      var $this = $(this);
+      if($rule == 'box-shadow'){
+         $this.css($rule,'0 0 1em ' + debugColor);
+      }
+      else if($rule == 'border'){
+         $this.css($rule,'1px solid ' + debugColor);
+      }
+      else{
+         $this.css($rule, debugColor);
+      }
+    })
+  } 
+  
+  debug('background');   
+})
