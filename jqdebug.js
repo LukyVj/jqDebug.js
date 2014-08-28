2fc76301dccd1dcfb0eae77595d364c758ea145c
Lucas Bonomi <lucas.bonomi@gmail.com>
2014-08-28 15:36:26 -0700

    Update jqdebug.js

--- jqdebug.js
+++ jqdebug.js
@@ -4,25 +4,26 @@
   Date : Aug 2014
 */
 
-$(function(){
+/* 
+  jqDebug.js | A tiny jquery debug function.
+  Author : @LukyVj
+  Date : Aug 2014
+*/
+
+function debug($rule){
+  var debug = $('[data-debug]');
+  var debugColor = debug.attr('data-debug');
   
-  function debug($rule){
-    var debug = $('[data-debug]');
-    var debugColor = debug.attr('data-debug');
-    
-    debug.each(function(){
-      var $this = $(this);
-      if($rule == 'box-shadow'){
-         $this.css($rule,'0 0 1em ' + debugColor);
-      }
-      else if($rule == 'border'){
-         $this.css($rule,'1px solid ' + debugColor);
-      }
-      else{
-         $this.css($rule, debugColor);
-      }
-    })
-  } 
-  
-  debug('background');   
-})
+  debug.each(function(){
+    var $this = $(this);
+    if($rule == 'box-shadow'){
+       $this.css($rule,'0 0 1em ' + debugColor);
+    }
+    else if($rule == 'border'){
+       $this.css($rule,'1px solid ' + debugColor);
+    }
+    else{
+       $this.css($rule, debugColor);
+    }
+  })
+} 
